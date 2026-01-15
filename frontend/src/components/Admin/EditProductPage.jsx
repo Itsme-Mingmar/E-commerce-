12a2907645666b2abc1baf2dbf3f58c6e3648106
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-01-15 17:54:41 +0545

    add image upload section in edit product page

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -10,8 +10,7 @@ const EditProductPage = () => {
     category: "",
     images: [
       {
-        url: "",
-        alt: ""
+        url: ""
       }
     ]
   })
@@ -22,7 +21,7 @@ const EditProductPage = () => {
     })
   }
   return (
-    <div className='min-h-screen bg-gray-100 md:px-20 px-8 py-6'>
+    <div className='min-h-screen bg-gray-100 md:px-20 px-8 pt-6 pb-16'>
       <h1 className='flex justify-center font-bold text-gray-700 text-xl border-b border-gray-300 py-4 mb-6 '>Edit Product</h1>
       <div>
         <form >
@@ -85,7 +84,33 @@ const EditProductPage = () => {
               </select>
             </div>
           </div>
-
+          <p className='font-medium text-gray-700 mb-1'>Images</p>
+          <div className='border-2 border-gray-300 px-3 py-3 rounded mb-4'>
+            <div className='flex gap-4 mb-6'>
+              <input
+                type="text"
+                name='url'
+                placeholder='Image URL'
+                value={formData.images.url}
+                onChange={handleInput}
+                className='w-full p-2 border border-gray-400 rounded'
+              />
+              <button className='bg-red-600 rounded text-white/90 px-3 cursor-pointer'>X</button>
+            </div>
+            <div className='flex gap-4 '>
+              <input
+                type="text"
+                name='url'
+                placeholder='Image URL'
+                value={formData.images.url}
+                onChange={handleInput}
+                className='w-full p-2 border border-gray-400 rounded'
+              />
+              <button className='bg-red-600 rounded text-white/90 px-3 cursor-pointer'>X</button>
+            </div>
+          </div>
+          <button className='border border-gray-400 rounded hover:bg-gray-200 w-full py-2 text-gray-700 font-medium cursor-pointer mb-6'>+ Add Image</button>
+          <button className='text-white/80 bg-amber-400 hover:bg-amber-500 rounded w-full py-2 font-medium cursor-pointer'>Update Product</button>
         </form>
       </div>
     </div>
