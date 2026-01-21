57bd00bc94cdade6d359a3628970251c959387fb
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-01-21 20:33:10 +0545

    add authslice for manage authication

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -2,3 +2,99 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from "axios" 
 
 // retive user info and token from localstorage 
+const userFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
+//check for the exiting gest id 
+const initialGuestId = localStorage.getItem("guestId") || `guest_${new Date(). getTime()}`;
+localStorage.setItem("guestId", initialGuestId)
+
+//initial state
+const initialState = {
+    user: userFromStorage,
+    guestId: initialGuestId,
+    loading: false,
+    error: null, 
+};
+// async thunk for user login
+export const loginUser = createAsyncThunk (
+    "auth/loginUser",
+    async(userData, {rejectWithValue}) => {
+        try{
+            const response = await axios.post(
+                `${import.meta.env.VITE_BACKEND_URL}/api/users/login`,
+                userData
+            );
+            localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+            localStorage.setItem("userToken", response.data.token);
+            return response.data.user;
+        } catch(error){
+            return rejectWithValue(error.response);
+        }
+    }
+)
+// async thunk for user Registration
+export const registerUser = createAsyncThunk (
+    "auth/loginUser",
+    async(userData, {rejectWithValue}) => {
+        try{
+            const response = await axios.post(
+                `${import.meta.env.VITE_BACKEND_URL}/api/users/register`,
+                userData
+            );
+            localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+            localStorage.setItem("userToken", response.data.token);
+            return response.data.user;
+        } catch(error){
+            return rejectWithValue(error.response);
+        }
+    }
+)
+
+//slice
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            state.guestId= `guest_${new Date().getTime()}`;
+            localStorage.removeItem("userInfo");
+            localStorage.removeItem("usertoken");
+            localStorage.setItemItem("guestId", state.guestId);
+        },
+        generateNewGuestId: (state)=>{
+            state.guestId = `guest_${new Date().getTime()}`;
+            localStorage.setItem("guestid", state.guestId);
+        }
+    },
+    extraReducers: (builder) =>{
+        builder.addCase(loginUser.pending, (state)=>{
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(loginUser.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        })
+        builder.addCase(loginUser.rejected, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload.message;
+        })
+        builder.addCase(registerUser.pending, (state)=>{
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(registerUser.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        })
+        builder.addCase(registerUser.rejected, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload.message;
+        })
+    }
+});
+export { logout, generateNewGuestId } = authSlice.actions;
+export default authSlice.reducer;
