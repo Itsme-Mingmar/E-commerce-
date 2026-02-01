434504bf49a53a83c5a4ea3e5abfe39f66c5deb8
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-02-01 22:37:38 +0545

    create checkout slice

---
+++ frontend/src/redux/slices/checkoutSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createCheckout = createAsyncThunk(
+    "checkout/createCheckout",
+    async (checkoutdata, { isRejectedWithValue }) => {
+        try {
+            const response = await axios.post(
+                `${import.meta.env.VITE_BACKEND_URL}/api/checkout`,
+                checkoutdata,
+                //  {
+
+                //     authorization
+                // }
+            )
+            return response.data;
+        } catch (error) {
+            return isRejectedWithValue(error.response.data);
+        }
+    }
+);
+
+const checkoutSlice = createSlice({
+    name: "checkout",
+    initialState: {
+        checkout: null,
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(checkoutSlice.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(checkoutSlice.fulfilled, (state, action) => {
+                state.loading = false;
+                state.checkout = action.payload;
+            })
+            .addCase(checkoutSlice.pending, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            });
+    },
+});
+export default checkoutSlice.reducer;
