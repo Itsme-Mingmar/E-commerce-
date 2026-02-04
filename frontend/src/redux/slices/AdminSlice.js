e3ca4f8d28d465542dbea840a0ac866b43aa9a21
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-02-04 21:15:35 +0545

    create adminSlice

--- frontend/src/redux/slices/AdminSlice.js
+++ frontend/src/redux/slices/AdminSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 //fetch all users
@@ -56,7 +56,7 @@ export const updateUser = createAsyncThunk(
 // Delete a user
 export const deleteUser = createAsyncThunk(
     "admin/deleteUser",
-    async(id) =>{
+    async (id) => {
         await axios.delete(
             `${import.meta.env.VITE_BACKEND_URL}/api/admin/user/${id}`,
             {
@@ -67,3 +67,59 @@ export const deleteUser = createAsyncThunk(
     }
 );
 
+const adminSlice = createSlice({
+    name: "admin",
+    initialState: {
+        users: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUsers.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                state.users = action.payload;
+                state.loading = false;
+                state.error = null;
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+            .addCase(updateUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                const updatedUser = action.payload;
+                const userIndex = state.users.findIndex((user) =>
+                    user._id === updatedUser._id
+                );
+                if (userIndex !== -1) {
+                    state.users[userIndex] = updateUser;
+                }
+            })
+            .addCase(deleteUser.fulfilled, (state, action) => {
+                state.users = state.users.filter((user) => user._id !== action.payload)
+            })
+            .addCase(addUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(addUser.fulfilled, (state, action) => {
+                state.users.push(action.payload.user)
+                state.loading = false;
+                state.error = null;
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            })
+    }
+
+});
+export default adminSlice.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,6 +4,7 @@ import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartslice"
 import checkoutReducer from "./slices/checkoutSlice"
 import orderReducer from"./slices/orderSlice"
+import adminReducer from "./slices/AdminSlice"
 
 const store = configureStore({
   reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
     cart: cartReducer,
     checkout: checkoutReducer,
     order: orderReducer,
+    admin: adminReducer
   },
 })
 export default store;
