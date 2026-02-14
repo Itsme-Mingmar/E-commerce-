c724eeb0109d6e929cf7fcaa19de37cbd0c7aac8
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-02-14 23:44:17 +0545

    add usedispatch and useselecter for similar products

--- frontend/src/components/Prouducts/SimilarProducts.jsx
+++ frontend/src/components/Prouducts/SimilarProducts.jsx
@@ -1,48 +1,44 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSimilarProducts } from "../../redux/slices/productSlice";
 import ProductCard from "./ProductCard"
 
-const SimilarProducts = ({ category }) => {
-  // Mock similar products
-  const products = [
-    {
-      _id: "2",
-      name: "Mass Gainer",
-      price: 5200,
-      category: "Protein",
-      images: [{ URL: "https://picsum.photos/400?1" }]
-    },
-    {
-      _id: "3",
-      name: "BCAA Energy",
-      price: 3200,
-      category: "Protein",
-      images: [{ URL: "https://picsum.photos/400?2" }]
-    },
-    {
-      _id: "3",
-      name: "BCAA Energy",
-      price: 3200,
-      category: "Protein",
-      images: [{ URL: "https://picsum.photos/400?2" }]
-    },
-    {
-      _id: "3",
-      name: "BCAA Energy",
-      price: 3200,
-      category: "Protein",
-      images: [{ URL: "https://picsum.photos/400?2" }]
-    }
-  ]
+const SimilarProducts = ({ id }) => {
+  const dispatch = useDispatch();
+  const { similarProducts, loading, error } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+  if (!id) return;   
+
+  dispatch(fetchSimilarProducts(id));
+
+}, [dispatch, id]);
+
+
+  if (loading) return <p>Loading similar products...</p>;
+  if (error)
+    return (
+      <div className="text-center py-20 text-red-500">
+        Error: {error}
+      </div>
+    );
 
   return (
     <section className="mt-20">
       <h2 className="text-2xl font-bold mb-8">
-        You May Also Like: 
+        You May Also Like:
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {products.map(product => (
-          <ProductCard key={product._id} product={product} />
-        ))}
+        {similarProducts?.length > 0 ? (
+          similarProducts.map(product => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        ) : (
+          <p>No similar products found.</p>
+        )}
       </div>
     </section>
   )
