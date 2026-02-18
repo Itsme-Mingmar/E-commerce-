950c33083675e3ee9f96974d9dd14807ae8fe61a
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-02-18 10:48:50 +0545

    token pass through cookies not through res

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -23,39 +23,46 @@ const userRegister = asyncHandler(async (req, res) => {
     if (!userCreated) {
         throw new apiError(500, "User is not created");
     }
+    const accessToken = accesstoken({ id: newUser._id, email: newUser.email });
+    const options = {
+        httpOnly: true,
+    }
     res
         .status(201)
+        .cookie("accessToken", accessToken, options)
         .json(new apiResponse(201, "User created successfully", userCreated));
 });
-const userLogin = asyncHandler(async(req, res)=>{
-    const {email, password} = req.body;
-    if(!email){
+const userLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
         throw new apiError(400, "Email required")
     }
-    const client = await user.findOne({email: email});
-    if(!client){
+    const client = await user.findOne({ email: email });
+    if (!client) {
         throw new apiError(401, "User not found")
     }
     const result = await client.isPasswordCorrect(password)
-    if(!result){
+    if (!result) {
         throw new apiError(400, "wrong password")
     }
-    const accessToken = accesstoken({id: client._id, email: client.email});
+    const accessToken = accesstoken({ id: client._id, email: client.email });
     const loginClient = await user.findById(client._id).select("-password").lean();
-    const options = { 
+    const options = {
         httpOnly: true,
-        secure: true
     }
     res.status(200)
-    .cookie("accessToken", accessToken, options)
-    .json( {
-        user: loginClient
-    },new apiResponse(200, "login successfully"))
+   .cookie("accessToken", accessToken, options)
+   .json(new apiResponse(
+       200,
+       loginClient,
+       "Login successfully"
+   ));
+
 
 })
-const userProfile = asyncHandler(async(req, res)=>{
+const userProfile = asyncHandler(async (req, res) => {
     const client = await user.findById(req.user?._id).select("-password")
     console.log(client);
     res.status(200).json(new apiResponse(200, client, "get user profile"))
 });
-export {userRegister, userLogin, userProfile};
+export { userRegister, userLogin, userProfile };
--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -24,10 +24,12 @@ export const loginUser = createAsyncThunk (
         try{
             const response = await axios.post(
                 `${import.meta.env.VITE_BACKEND_URL}/api/userLogin`,
-                userData
+                userData,
+                {
+                    withCredentials: true,
+                }
             );
             localStorage.setItem("userInfo", JSON.stringify(response.data.data));
-            localStorage.setItem("userToken", response.data.token);
             return response.data.data;
         } catch(error){
             return rejectWithValue(error.response);
@@ -41,10 +43,12 @@ export const registerUser = createAsyncThunk (
         try{
             const response = await axios.post(
                 `${import.meta.env.VITE_BACKEND_URL}/api/userRegister`,
-                userData
+                userData,
+                {
+                    withCredentials: true,
+                }
             );
             localStorage.setItem("userInfo", JSON.stringify(response.data.data));
-            localStorage.setItem("userToken", response.data.token);
             return response.data.data;
         } catch(error){
             return rejectWithValue(error.response);
@@ -61,12 +65,11 @@ const authSlice = createSlice({
             state.user = null;
             state.guestId= `guest_${new Date().getTime()}`;
             localStorage.removeItem("userInfo");
-            localStorage.removeItem("userT oken");
             localStorage.setItem("guestId", state.guestId);
         },
         generateNewGuestId: (state)=>{
             state.guestId = `guest_${new Date().getTime()}`;
-            localStorage.setItem("guestid", state.guestId);
+            localStorage.setItem("guestId", state.guestId);
         }
     },
     extraReducers: (builder) =>{
