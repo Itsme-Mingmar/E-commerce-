3b4c6a1fbdb9c986695a07deeb59e6bde5826404
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-05-20 17:52:00 +0545

    fix(cart): change addToCart to use PUT method and update error handling in mergeCart

--- frontend/src/redux/slices/cartslice.js
+++ frontend/src/redux/slices/cartslice.js
@@ -41,7 +41,7 @@ export const addToCart = createAsyncThunk(
     "cart/addToCart",
     async ({ productId, quantity, guestId }, { rejectWithValue }) => {
         try {
-            const response = await axios.post(
+            const response = await axios.put(
                 `${import.meta.env.VITE_BACKEND_URL}/api/addToCart`,
                 {
                     productId,
@@ -96,7 +96,7 @@ export const mergeCart = createAsyncThunk(
     "cart/mergeCart",
     async ({ guestId}, { rejectWithValue }) => {
         try {
-            const response = axios.post(
+            const response = await axios.post(
                 `${import.meta.env.VITE_BACKEND_URL}/api/mergeCart`,
                 { guestId },
                 {
@@ -105,7 +105,7 @@ export const mergeCart = createAsyncThunk(
             )
             return await response.data.data;
         } catch (error) {
-            return rejectWithValue(error.response.data.message);
+            return rejectWithValue(error.response?.data?.message);
         }
     }
 )
