06e5222688e9ea6685e66945a1c678d7000b3e2d
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-05-21 23:44:55 +0545

    fix(app): add cookieParser middleware to express app

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ import adminRouter from './Router/adminRouter.js';
 import orderMngRouter from './Router/orderManagementRouter.js';
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
